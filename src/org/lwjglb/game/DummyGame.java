4a5c44085ab98c4329e0099fd948798d2c49c699
NatiG100 <kalebok39@gmail.com>
2022-03-10 00:07:44 -0800

    cube mesh

--- src/org/lwjglb/game/DummyGame.java
+++ src/org/lwjglb/game/DummyGame.java
@@ -27,19 +27,46 @@ public class DummyGame implements IGameLogic{
     public void init(Window window) throws Exception{
         renderer.init(window);
         float[] positions = new float[]{
-                -0.5f,0.5f,-1.5f,
-                -0.5f,-0.5f,-1.5f,
-                0.5f,-0.5f,-1.5f,
-                0.5f,0.5f,-1.5f,
+                // VO
+                -0.5f,  0.5f,  0.5f,
+                // V1
+                -0.5f, -0.5f,  0.5f,
+                // V2
+                0.5f, -0.5f,  0.5f,
+                // V3
+                0.5f,  0.5f,  0.5f,
+                // V4
+                -0.5f,  0.5f, -0.5f,
+                // V5
+                0.5f,  0.5f, -0.5f,
+                // V6
+                -0.5f, -0.5f, -0.5f,
+                // V7
+                0.5f, -0.5f, -0.5f,
         };
         float[] colours = new float[]{
                 0.5f, 0.0f, 0.0f,
-                0.0f,0.5f,0.0f,
-                0.0f,0.0f,0.5f,
-                0.0f,0.5f,0.5f
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f,
+                0.5f, 0.0f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f,
         };
         int[] indices = new int[]{
-                0,1,3,3,1,2
+                // Front face
+                0, 1, 3, 3, 1, 2,
+                // Top Face
+                4, 0, 3, 5, 4, 3,
+                // Right face
+                3, 2, 7, 5, 3, 7,
+                // Left face
+                6, 1, 0, 6, 0, 4,
+                // Bottom face
+                2, 1, 6, 2, 6, 7,
+                // Back face
+                7, 6, 4, 7, 4, 5,
         };
         Mesh mesh=new Mesh(positions,colours,indices);
         GameItem gameItem = new GameItem(mesh);
